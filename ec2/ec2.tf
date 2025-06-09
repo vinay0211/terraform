380da81994c0cae6212676b468e6d0e01f47c5f0
VINAY <vinaykumarreddy001@gmail.com>
2025-06-09 18:16:11 +0530

    Terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "roboshop" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+    name        = "allow_all"
+    description = "allow all traffic"
+
+    ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+        Name = "allow-all"
+    }
+}
---
+++ variables/variable.tf
@@ -0,0 +1,39 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "AMI ID to join devops"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "ec2_tags"  {
+    type = map(string)
+    default = {
+    name = "HelloWorld"
+    purpose = "variable-demo"
+    }
+}
+variable "sg_name" {
+    default = "allow_all"
+}
+variable "sg_description" {
+    default = "Allowing all ports from all over the Internet"
+}
+variable "from_port"{
+    default=0
+}
+variable "to_port" {
+    default=0
+}
+variable "cidr_blocks" {
+    default = ["0.0.0.0/0"]
+    type = list(string)
+}
+variable "sg_tags" {
+    default = {
+        name= "allow_all"
+    }
+}
