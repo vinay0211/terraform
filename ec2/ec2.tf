bda53fc096e43c4d4d9e4e5e2689ed8b8e66b7db
VINAY <vinaykumarreddy001@gmail.com>
2025-06-19 13:47:22 +0530

    hello world started

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "roboshop" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
-  
+  #vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+  vpc_security_group_ids = local.sg_id
   tags = {
     Name = "HelloWorld"
   }
