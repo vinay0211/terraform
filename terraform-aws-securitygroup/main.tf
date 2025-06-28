9e99a46ea77e5f2a8f0baba3397ecc76474a1096
VINAY <vinaykumarreddy001@gmail.com>
2025-06-28 17:26:55 +0530

    hello world started

---
+++ terraform-aws-securitygroup/main.tf
@@ -0,0 +1,22 @@
+#roboshop-dev-frontend
+resource "aws_security_group" "main" {
+  name        = var.sg_name
+  description = var.sg_description
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.sg_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-${var.sg_name}"
+    }
+  )
+}
--- vpc-module-test/provider.tf
+++ vpc-module-test/provider.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.98.0"
     }
   }
 
   backend "s3" {
-    bucket = "84s-remotestate"
-    key    = "vpc-test"
-    region = "us-east-1"
-    encrypt        = true
+    bucket       = "84s-remotestate"
+    key          = "vpc-test"
+    region       = "us-east-1"
+    encrypt      = true
     use_lockfile = true
   }
 }
